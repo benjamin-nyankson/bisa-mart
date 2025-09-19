e2a016b532812a3fce056bd418117f4becb1ce06
Benjamin Nyankson <nyanksonbenjamin4@gmail.com>
2025-09-19 18:06:58 +0000

    fix api fetch

--- components/constant.ts
+++ components/constant.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://195.35.36.122:1991/api/authentication";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://195.35.36.122:1991/api/authentication" // local/dev direct IP
+    : "/api/authentication"; // prod will go through proxy
